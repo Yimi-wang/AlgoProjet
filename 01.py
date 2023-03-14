99d1adc583543e11a0156ff40a42ca1d9ab2a9d4
Yimi-wang <wmt200802@aliyun.com>
2023-03-14 11:47:46 +0100

    Structure et add_class

---
+++ 01.py
@@ -0,0 +1,31 @@
+class KNNClasses:
+    def __init__(self,description,data) -> None:
+        self.description =description 
+        '''
+        [  
+	{ 
+		"label": "XXX_1",
+		"vectors": [
+			{ "key_1_1_1": float_1_1_1, "key_1_1_2":float_1_1_2,... },
+			{ "key_1_2_1": float_1_2_1, "key_1_2_2":float_1_2_2,... },
+			...
+	},
+	{ 
+		"label": "XXX_2",
+		"vectors": [
+			{ "key_2_1_1": float_2_1_1, "key_2_1_2":float_2_1_2,... },
+			{ "key_2_2_1": float_2_2_1, "key_2_2_2":float_2_2_2,... },
+			...
+	},
+	...
+]
+    一个list，里面的元素是dict
+        '''
+        self.data=data
+
+    def add_class(self,label:str,vectors:list) -> None:
+        add_element={"label":label,"vectors":vectors}
+        self.data.add(add_element)
+
+    def add_vector(self,label:str,vector:list) -> None:
+        pass
